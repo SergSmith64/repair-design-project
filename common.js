9ab1de5ad8811ee695e98ed89a0a489f22cd8baf
SergSmith64 <sergsmith2008@yandex.ru>
2019-10-19 17:28:30 +0400

    feat: Luffi slider

--- common.js
+++ common.js
@@ -1,1 +1,24 @@
 const firstButton = document.getElementById('button-1');
+const secondButton = document.getElementById('button-2');
+const thirdButton = document.getElementById('button-3');
+// const fourthButton = document.getElementById('button-4');
+
+firstButton.addEventListener('click', () =>  {
+  const content = document.getElementById('slider-content')
+  content.className = "first-position"
+})
+
+secondButton.addEventListener('click', () =>  {
+  const content = document.getElementById('slider-content')
+  content.className = "second-position"
+})
+
+thirdButton.addEventListener('click', () =>  {
+  const content = document.getElementById('slider-content')
+  content.className = "third-position"
+})
+
+// fourthButton.addEventListener('click', () =>  {
+//   const content = document.getElementById('slider-content')
+//   content.className = "fourth-position"
+// })
