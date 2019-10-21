22c478e507e0ed7c744f3e0ccfbe11543bd1ebae
SergSmith64 <sergsmith2008@yandex.ru>
2019-10-22 02:12:20 +0400

    feat: add buttons

--- common.js
+++ common.js
@@ -1,7 +1,6 @@
 const firstButton = document.getElementById('button-1');
 const secondButton = document.getElementById('button-2');
 const thirdButton = document.getElementById('button-3');
-// const fourthButton = document.getElementById('button-4');
 
 firstButton.addEventListener('click', () =>  {
   const content = document.getElementById('slider-content')
@@ -17,8 +16,3 @@ thirdButton.addEventListener('click', () =>  {
   const content = document.getElementById('slider-content')
   content.className = "third-position"
 })
-
-// fourthButton.addEventListener('click', () =>  {
-//   const content = document.getElementById('slider-content')
-//   content.className = "fourth-position"
-// })
